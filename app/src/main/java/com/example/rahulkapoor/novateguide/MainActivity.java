d24c0f65d6c5c81accef4a821fd3c5ad00a0fff8
Rahul Kapoor <rahul253801@gmail.com>
2018-02-25 14:43:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rahulkapoor/novateguide/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rahulkapoor.novateguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
