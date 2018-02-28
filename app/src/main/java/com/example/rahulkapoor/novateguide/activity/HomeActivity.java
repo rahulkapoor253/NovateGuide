c1cf97e377721b1359b92be82667c8756199e216
Rahul Kapoor <rahul253801@gmail.com>
2018-02-28 21:05:37 +0530

    places appbar, drawer layout implemented

--- app/src/main/java/com/example/rahulkapoor/novateguide/activity/HomeActivity.java
+++ app/src/main/java/com/example/rahulkapoor/novateguide/activity/HomeActivity.java
@@ -1,15 +1,22 @@
 package com.example.rahulkapoor.novateguide.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.rahulkapoor.novateguide.R;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ImageView ivAddMarker, ivDrawer;
+    private ImageView ivAddMarker, ivDrawer, ivUserImage;
+    private DrawerLayout drawerLayout;
+    private TextView tvUsername, tvEmail;
+    private static final int REQ_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,11 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 //open close the drawer;
+                if (drawerLayout.isDrawerOpen(Gravity.START)) {
+                    drawerLayout.closeDrawer(Gravity.END);
+                } else {
+                    drawerLayout.openDrawer(Gravity.START);
+                }
             }
         });
 
@@ -30,8 +42,22 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(final View v) {
                 //take user to places list screen and get the result from it;
 
+                Intent i = new Intent(HomeActivity.this, PlacesActivity.class);
+                startActivityForResult(i, REQ_CODE);
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQ_CODE) {
+                //perform the necessary option on callback;
+
+            }
+        }
     }
 
     /**
@@ -41,6 +67,10 @@ public class HomeActivity extends AppCompatActivity {
 
         ivAddMarker = (ImageView) findViewById(R.id.iv_add_marker);
         ivDrawer = (ImageView) findViewById(R.id.iv_drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        tvEmail = (TextView) findViewById(R.id.tv_email);
+        tvUsername = (TextView) findViewById(R.id.tv_username);
+        ivUserImage = (ImageView) findViewById(R.id.iv_user);
 
     }
 }
