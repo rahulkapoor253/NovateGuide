3105f78507facb17a541d40dfd07429c6d57db6c
Rahul Kapoor <rahul253801@gmail.com>
2018-02-28 20:20:16 +0530

    custom appbar implemented

--- app/src/main/java/com/example/rahulkapoor/novateguide/activity/HomeActivity.java
+++ app/src/main/java/com/example/rahulkapoor/novateguide/activity/HomeActivity.java
@@ -2,14 +2,45 @@ package com.example.rahulkapoor.novateguide.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.rahulkapoor.novateguide.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView ivAddMarker, ivDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        init();
+
+        ivDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //open close the drawer;
+            }
+        });
+
+        ivAddMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //take user to places list screen and get the result from it;
+
+            }
+        });
+    }
+
+    /**
+     * init made;
+     */
+    private void init() {
+
+        ivAddMarker = (ImageView) findViewById(R.id.iv_add_marker);
+        ivDrawer = (ImageView) findViewById(R.id.iv_drawer);
+
     }
 }
