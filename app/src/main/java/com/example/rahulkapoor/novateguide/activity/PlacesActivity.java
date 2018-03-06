ff14a168cab80ec93472ac2b9094a8c0880e29cb
Rahul Kapoor <rahul253801@gmail.com>
2018-03-06 23:06:22 +0530

    places item view, adapter implemented

--- app/src/main/java/com/example/rahulkapoor/novateguide/activity/PlacesActivity.java
+++ app/src/main/java/com/example/rahulkapoor/novateguide/activity/PlacesActivity.java
@@ -1,15 +1,86 @@
 package com.example.rahulkapoor.novateguide.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 import com.example.rahulkapoor.novateguide.R;
+import com.example.rahulkapoor.novateguide.adapter.PlacesAdapter;
+
+import java.util.ArrayList;
 
 public class PlacesActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
+    private RecyclerView rvPlaces;
+    private PlacesAdapter placesAdapter;
+    private ArrayList<String> placesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+
+        init();
+
+        addDataToList();
+
+        setAdapter();
+
+    }
+
+    /**
+     * adding data to array list;
+     */
+    private void addDataToList() {
+
+        placesList.add("Gallileo Block");
+        placesList.add("Newton Block");
+        placesList.add("Demorgan Block");
+        placesList.add("Chitkara Woods");
+        placesList.add("Exploretorium");
+        placesList.add("Turing Block");
+        placesList.add("Indoor Sports");
+        placesList.add("Teresa Girl Hostel");
+        placesList.add("Bloom's Building");
+        placesList.add("Main Ground");
+        placesList.add("CSHS Park(Gallileo)");
+        placesList.add("PNB ATM");
+        placesList.add("Chitkara School Of Planning and Architecture");
+        placesList.add("Columbus Hostel");
+        placesList.add("Vasco Da Gama Hostel");
+        placesList.add("Central Library");
+        placesList.add("Babbage Block");
+        placesList.add("Chitkara School Of Hospitality");
+        placesList.add("Square One Cafeteria");
+        placesList.add("Hostel Ground");
+        placesList.add("Pie A Hostel");
+        placesList.add("Pie B Hostel");
+        placesList.add("Pie C Hostel");
+        placesList.add("Tuck Shop");
+
+    }
+
+    /**
+     * initialised;
+     */
+    private void init() {
+
+        ivBack = (ImageView) findViewById(R.id.iv_back);
+        rvPlaces = (RecyclerView) findViewById(R.id.rv);
+
+    }
+
+    /**
+     * setting up the places adapter;
+     */
+    private void setAdapter() {
+
+        placesAdapter = new PlacesAdapter(PlacesActivity.this, placesList);
+        rvPlaces.setLayoutManager(new LinearLayoutManager(PlacesActivity.this, LinearLayoutManager.VERTICAL, false));
+        rvPlaces.setAdapter(placesAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/rahulkapoor/novateguide/adapter/PlacesAdapter.java
@@ -0,0 +1,73 @@
+package com.example.rahulkapoor.novateguide.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.rahulkapoor.novateguide.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rahulkapoor on 06/03/18.
+ */
+
+public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.PlaceViewHolder> {
+
+    private Context mContext;
+    private ArrayList<String> placesList = new ArrayList<>();
+
+    public PlacesAdapter(final Context context, final ArrayList<String> list) {
+
+        this.mContext = context;
+        this.placesList = list;
+
+    }
+
+    @Override
+    public PlacesAdapter.PlaceViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        View view = LayoutInflater.from(
+                parent.getContext()).inflate(R.layout.places_list_item, parent, false);
+
+        return new PlacesAdapter.PlaceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final PlacesAdapter.PlaceViewHolder holder, final int position) {
+
+        holder.tvPlaceItem.setText(placesList.get(position));
+
+        holder.llItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //return the index and string for setting up marker and lat/lng;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return placesList.size();
+    }
+
+
+    public class PlaceViewHolder extends RecyclerView.ViewHolder {
+
+        private LinearLayout llItem;
+        private TextView tvPlaceItem;
+
+        public PlaceViewHolder(final View itemView) {
+            super(itemView);
+
+            llItem = (LinearLayout) itemView.findViewById(R.id.ll_item);
+            tvPlaceItem = (TextView) itemView.findViewById(R.id.tv_item_place);
+
+        }
+    }
+}
